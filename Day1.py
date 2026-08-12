ee5d8e326604094315b35fa564a447a8a7fa15e5
Mahesh Kshirsagar <kshirsagarmahesh108@gmail.com>
2026-08-12 07:16:36 +0530

    this is my day 1

---
+++ Day1.py
@@ -0,0 +1,28 @@
+# print("Welcome to My DSA Learning Journey")
+
+# # Data Types:
+
+# pi=3.14
+# print(type("mahesh"))
+# print(type(55))
+# print(type(True))
+# print(type(pi))
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# a=5
+# b=6
+# sum = a+b
+# print("sum=",sum)
+
+# c=int(input("enter first value:"))
+# d=int(input("enter second value:"))
+# sum1=c+d
+# print("sum=",sum1)
+
+#--------------------------------------------------------------------------------------------------------------------
+
+a=float(input("enter value:"))
+b=float(input("enter value:"))
+avg=(a+b)/2
+print(avg)
